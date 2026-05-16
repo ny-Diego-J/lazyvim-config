546ad93981c5dce49bf5d707fa0294aac1b6c17b
Diego Jimenez <diego.jimenez@noeryoung.ch>
2026-05-16 10:49:27 +0200

    fix markdown plugin

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -2,7 +2,8 @@ vim.api.nvim_create_autocmd("LspAttach", {
     callback = function(args)
         local client = vim.lsp.get_client_by_id(args.data.client_id)
         if client and client.name == "marksman" then
-            vim.diagnostic.enisable(false, args.buf)
+            vim.diagnostic.enable(false, { bufnr = vim.api.nvim_get_current_buf() })
+            vim.opt_local.spell = false
         end
     end,
 })
