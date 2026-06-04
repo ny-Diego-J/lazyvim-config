6f015082f29c582cd1a9ff168ad9019ad0a123a3
Diego Jimenez <diego.jimenez@noeryoung.ch>
2026-06-04 21:58:56 +0200

    dissable supermaven by default

--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -1,5 +1,7 @@
 return {
     "supermaven-inc/supermaven-nvim",
+    enabled = false,
+    lazy = true,
     config = function()
         require("supermaven-nvim").setup({
             keymaps = {
@@ -7,10 +9,17 @@ return {
                 clear_suggestion = "<C-]>",
                 accept_word = "<C-j>",
             },
-            ignore_filetypes = { cpp = true },
-            log_level = "info",
-            disable_inline_completion = false,
-            disable_keymaps = false,
+            ignore_filetypes = { cpp = true }, -- or { "cpp", }
+            color = {
+                suggestion_color = "#ffffff",
+                cterm = 244,
+            },
+            log_level = "info", -- set to "off" to disable logging completely
+            disable_inline_completion = false, -- disables inline completion for use with cmp
+            disable_keymaps = false, -- disables built in keymaps for more manual control
+            condition = function()
+                return false
+            end, -- condition to check for stopping supermaven, `true` means to stop supermaven when the condition is true.
         })
     end,
 }
